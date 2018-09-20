4ad380b8bf15b76e1fa852949a86fb1401f61c93
mattwalo32 <mattwalowski@gmail.com>
2018-09-20 15:06:58 -0500

    Added Adjust Motor Speed Method

--- Arduino Programs/Autonomous/Autonomous.ino
+++ Arduino Programs/Autonomous/Autonomous.ino
@@ -5,8 +5,8 @@ int liftMotorPinRight = 6;
 
 // The value of of lift offset will provide a factor of more power
 // to one motor over another. Positive favors right, negative left.
-final double LIFT_OFFSET = 0.0;
-final double THRUST_OFFSET = 0.0;
+double LIFT_OFFSET = 0.0;
+double THRUST_OFFSET = 0.0;
 
 // First method called for any initialization
 void setup() {
@@ -20,7 +20,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   rampLiftMotors(150);
-  driveStraight();
+  driveStraight(5000, 100);
 }
 
 /**
@@ -30,8 +30,8 @@ void loop() {
  */
 void driveStraight(int duration, int speedValue){
   // Set both motors to the same speed
-  analogWrite(thrustMotorPinLeft, speedValue);
-  analogWrite(thrustMotorPinRight, speedValue);
+  analogWrite(thrustMotorPinLeft, getAdjustedMotorSpeed(thrustMotorPinLeft, speedValue));
+  analogWrite(thrustMotorPinRight, getAdjustedMotorSpeed(thrustMotorPinRight, speedValue));
 
   // While duration has not elasped
   delay(duration);
@@ -44,20 +44,29 @@ void driveStraight(int duration, int speedValue){
  */
 void rampLiftMotors(int motorSpeed){
   int currentSpeed = 0; 
-  //Only use left side multipler if value less than 0
-  double leftMultiplier = LIFT_OFFSET < 0 ? liftOffset * -1 : 1;
-  //Only use right side multipler if value greater than 0
-  double rightMultiplier = LIFT_OFFSET > 0 ? liftOffset : 1;
 
   //While it is still accelerating
-  while(currentSpeed < speedValue){
-    analogWrite(liftMotorPinLeft, speedValue * leftMultiplier);
-    analogWrite(liftMotorPinRight, speedValue * rightMultiplier);
+  while(currentSpeed < motorSpeed){
+    analogWrite(liftMotorPinLeft, getAdjustedMotorSpeed(liftMotorPinLeft, currentSpeed));
+    analogWrite(liftMotorPinRight, getAdjustedMotorSpeed(liftMotorPinRight, currentSpeed));
     currentSpeed += 1;
     delay(10);
   }
+}
 
-  int getAdjustedMotorSpeed(int pinNum, int motorSpeed){
-    
+int getAdjustedMotorSpeed(int pinNum, int motorSpeed){
+  switch(pinNum){
+    case thrustMotorPinLeft:
+      return THRUST_OFFSET < 0 ? motorSpeed * THRUST_OFFSET * -1 : motorSpeed;
+      break;
+      case thrustMotorPinRight:
+    return THRUST_OFFSET > 0 ? motorSpeed * THRUST_OFFSET : motorSpeed;
+      break;
+    case liftMotorPinLeft:
+      return LIFT_OFFSET < 0 ? motorSpeed * THRUST_OFFSET * -1 : motorSpeed;
+      break;
+    case liftMotorPinRight:
+      return LIFT_OFFSET > 0 ? motorSpeed * THRUST_OFFSET : motorSpeed;
+      break;
   }
 }
